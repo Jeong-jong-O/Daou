e3e54a72da68b2de1879bcbf2bae4afa3d41907d
Jeong-jong-O <123707748+Jeong-jong-O@users.noreply.github.com>
2023-02-03 15:31:12 +0900

    Add files via upload

---
+++ project/pkg/Calculator.py
@@ -0,0 +1,9 @@
+class Calculator:
+    def __init__(self, title):
+        self.title = 'Cal module'
+        
+    def plus(x, y):
+        return x + y
+    
+    def minus(x, y):
+        return x - y
